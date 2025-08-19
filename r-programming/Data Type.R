859acc72a72dd9457f6eb52cd478f2a2aaed5e22
Suchanya Jaichun <150454529+suchanyajai@users.noreply.github.com>
2025-08-19 08:23:05 +0700

    lerning about data type in R Programing
    1. Nameric
    2. Character
    3. Logical 
    4. Factor
    5. Date/Time

---
+++ r-programming/Data Type.R
@@ -0,0 +1,29 @@
+## Data Types
+
+## 1. nameric
+age <- 32
+print(age)
+class(age)
+
+## 2. character
+my_name <- "Ploy"
+my_university <- 'NIDA'
+print(my_name)
+print(my_university)
+print(my_name);class(my_university)
+
+## 3. logical
+result <- 1 + 1 == 2
+print(result)
+class(result)
+
+## 4. factor
+animals <- c("Dog","Cat","Cat","Hippo")
+class(animals)
+
+animals <- factor(animals)
+class(animals)
+
+## 5. date/time
+time_now <- Sys.time()
+class(time_now)
